5a56119cc8a16535f7510dac94fbd6dca179a024
Vaycent <vaycentsun@gmail.com>
2016-09-12 11:50:54 +0800

    sync

--- app/src/main/java/vaycent/ormlitesharp/OrmliteSharp.java
+++ app/src/main/java/vaycent/ormlitesharp/OrmliteSharp.java
@@ -1,123 +1,63 @@
 package vaycent.ormlitesharp;
 
 import android.content.Context;
-import android.database.sqlite.SQLiteDatabase;
-import android.util.Log;
 
-import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
 import com.j256.ormlite.dao.Dao;
-import com.j256.ormlite.support.ConnectionSource;
-import com.j256.ormlite.table.TableUtils;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
- * Created by Vaycent on 16/9/10.
+ * Created by Vaycent on 16/9/12.
  */
+public class OrmliteSharp {
 
-public class OrmliteSharp extends OrmLiteSqliteOpenHelper
-{
-    private static final String TAG="OrmliteDatabaseHelper";
-    private static OrmliteSharp instance;
+    private ArrayList<Class> classList;
+    private String dbName = "";
+    private int dbVersion= 0;
+    private Context context;
 
-    private Map<String, Dao> daos = new HashMap<String, Dao>();
-
-    private static ArrayList<Class> classList;
-    private static String dbName = "";
-    private static int dbVersion= 0;
+    private OrmliteSharpHelper osHelper;
 
 
 
-    public void init(String dbName, int dbVersion, ArrayList<Class> classList){
-        this.classList=classList;
-        this.dbName=dbName;
-        this.dbVersion=dbVersion;
+    public OrmliteSharp(Context ct, String name, int version, ArrayList<Class> cl){
+        this.context=ct;
+        this.dbName=name;
+        this.dbVersion=version;
+        this.classList=cl;
+
+        OrmliteSharpHelper.initParameter(dbName,dbVersion,classList);
+
     }
 
-
-    private OrmliteSharp(Context context, String dbName, int dbVersion) {
-        super(context, dbName, null, dbVersion);
-    }
-
-    @Override
-    public void onCreate(SQLiteDatabase database, ConnectionSource connectionSource) {
-        try
-        {
-            for(int i=0;i<classList.size();i++){
-                Class classObj=classList.get(i).getClass();
-                TableUtils.createTable(connectionSource, classObj);
-            }
-        } catch (SQLException e)
+    private Dao getOS_Dao(Class daoClass){
+        osHelper = OrmliteSharpHelper.getHelper(context);
+        Dao dao=null;
+        try{
+            dao=osHelper.getDao(daoClass);
+        }catch (SQLException e)
         {
             e.printStackTrace();
-        }
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase database,
-                          ConnectionSource connectionSource, int oldVersion, int newVersion)
-    {
-        try
-        {
-            for(int i=0;i<classList.size();i++){
-                Class classObj=classList.get(i).getClass();
-                TableUtils.dropTable(connectionSource, classObj, true);
-            }
-            onCreate(database, connectionSource);
-        } catch (SQLException e)
-        {
-            e.printStackTrace();
-        }
-    }
-
-
-    public static synchronized OrmliteSharp getHelper(Context context) {
-        if (instance == null)
-        {
-            synchronized (OrmliteSharp.class)
-            {
-                if (instance == null)
-                    if(dbName.equalsIgnoreCase("")||dbVersion==0){
-                        Log.e(TAG,"You should use init() function to setup dbName and dbVersion before you getHelper");
-                    }else{
-                        instance = new OrmliteSharp(context,dbName,dbVersion);
-                    }
-            }
-        }
-        return instance;
-    }
-
-
-    @Override
-    public void close()
-    {
-        super.close();
-
-        for (String key : daos.keySet())
-        {
-            Dao dao = daos.get(key);
-            dao = null;
-        }
-    }
-
-    public synchronized Dao getDao(Class clazz) throws SQLException
-    {
-        Dao dao = null;
-        String className = clazz.getSimpleName();
-
-        if (daos.containsKey(className))
-        {
-            dao = daos.get(className);
-        }
-        if (dao == null)
-        {
-            dao = super.getDao(clazz);
-            daos.put(className, dao);
         }
         return dao;
+    }
+
+
+    public Context getContext(){
+        return context;
+    }
+
+    public String getDbName(){
+        return dbName;
+    }
+
+    public int getDbVersion(){
+        return dbVersion;
+    }
+
+    public ArrayList<Class> getClassList(){
+        return classList;
     }
 
 
